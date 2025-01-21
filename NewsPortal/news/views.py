fcca1887dda4d20a1da41b9da514832152c8ad56
chuunyaka <viktoriakasenceva95@gmail.com>
2025-01-21 17:51:53 +0300

    added shell_commands.txt

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView, DetailView
+from .models import Post
 
+
+class NewsList(ListView):
+    model = Post
+    ordering = 'title'
+    template_name = 'news.html'
+    context_object_name = 'news'
 # Create your views here.
+class NewDetail(DetailView):
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'new.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'new'
