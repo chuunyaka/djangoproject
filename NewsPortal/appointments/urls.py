42f652a49238f353e7d2b151652912d5079d2f28
chuunyaka <viktoriakasenceva95@gmail.com>
2025-07-12 23:35:21 +0300

    added shell_commands.txt added new information, update,news list and separate pages for full information about the articles

--- NewsPortal/appointments/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import index1  # Убедись, что представление index есть
-
-urlpatterns = [
-    path('', index1, name='index1'),
-]
--- NewsPortal/protect/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import IndexView
-# from .views import upgrade_me
-
-urlpatterns = [
-    path('', IndexView.as_view()),
-]
